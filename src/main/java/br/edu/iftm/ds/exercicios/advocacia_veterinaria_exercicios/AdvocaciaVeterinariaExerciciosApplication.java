d9e22d1103d29e39000158eab83e5ad331b7a4cd
Ingryd-julia <163544007+Ingryd-julia@users.noreply.github.com>
2024-08-18 02:30:07 -0300

    first commit

---
+++ src/main/java/br/edu/iftm/ds/exercicios/advocacia_veterinaria_exercicios/AdvocaciaVeterinariaExerciciosApplication.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.ds.exercicios.advocacia_veterinaria_exercicios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvocaciaVeterinariaExerciciosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvocaciaVeterinariaExerciciosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/iftm/ds/exercicios/advocacia_veterinaria_exercicios/AdvocaciaVeterinariaExerciciosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.ds.exercicios.advocacia_veterinaria_exercicios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvocaciaVeterinariaExerciciosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
